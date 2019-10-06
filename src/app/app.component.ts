69b95b2c44d429dc81093eb9be30523006d9789c
Alisson Youssf <youssfbr@gmail.com>
2019-10-06 08:24:25 -0300

    adicionado funcao uploadArquivo()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fileserver-ui';
+
+  uploadArquivo(event) {
+    console.log(event);
+  }
+
 }
